eb7caed7aad1b153941fd7e93aa610bc637c2cfb
Carl Moonan <carl.moonan@gmail.com>
2015-04-24 10:07:22 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks'}
+
+  get 'pages/index'
+  get '/admin' => 'admin/base#index', :as => 'admin'
+
+  namespace 'admin' do
+    resources :users
+  end
+
+  root :to => 'pages#index'
+  
+end
